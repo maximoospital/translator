d00d32347a1a595d2a5f1d5f054db7f8bfbc1138
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2025-05-02 02:31:10 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+import { defineNuxtConfig } from 'nuxt/config'
+
+const repositoryName = 'translator' // Your GitHub repository subfolder name
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: `/${repositoryName}/`,
+  },
+  vite: {
+    base: `/${repositoryName}/`,
+  },
 
   routeRules: {
-    // prerender index route by default
     '/': { prerender: true },
   },
 
+  // Ensure compatibility date 
   compatibilityDate: '2024-08-11',
 });
+
+
